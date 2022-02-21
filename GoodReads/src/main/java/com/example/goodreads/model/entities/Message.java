fbfd0be1f7525173b0c0abe8ccc32b06996eb33a
Sanya Sevdeva <s.g.sevdeva@gmail.com>
2022-02-21 14:44:57 +0200

    Edited messages table and entity

--- GoodReads/src/main/java/com/example/goodreads/model/entities/Message.java
+++ GoodReads/src/main/java/com/example/goodreads/model/entities/Message.java
@@ -6,34 +6,12 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "messages")
 public class Message {
-    public enum MessageFolder{
-        INBOX('i'), SAVED('s'), SENT('f'), TRASH('t');
-
-        public final char symbol;
-        MessageFolder(char symbol){
-            this.symbol = symbol;
-        }
-
-        public static boolean isValidGender(char symbol) {
-            return (symbol == INBOX.symbol ||
-                    symbol == SAVED.symbol ||
-                    symbol == SENT.symbol ||
-                    symbol == TRASH.symbol);
-        }
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long messageId;
 
     @Column
     private LocalDate sentAt;
-
-    @Column
-    private Boolean isRead;
-
-    @Column
-    private char receiverFolder;
 
     @ManyToOne
     @JoinColumn(name = "sender_id", referencedColumnName = "user_id")
