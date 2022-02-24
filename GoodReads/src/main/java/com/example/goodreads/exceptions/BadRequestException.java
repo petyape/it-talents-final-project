5d3f731db33217e12f26d416d512b849e66e63b7
Sanya Sevdeva <s.g.sevdeva@gmail.com>
2022-02-24 14:47:46 +0200

    Refactoring: Exceptions

--- GoodReads/src/main/java/com/example/goodreads/exceptions/BadRequestException.java
+++ GoodReads/src/main/java/com/example/goodreads/exceptions/BadRequestException.java
@@ -1,7 +1,9 @@
 package com.example.goodreads.exceptions;
 
 public class BadRequestException extends RuntimeException {
+
     public BadRequestException(String msg){
         super(msg);
     }
+
 }
--- GoodReads/src/main/java/com/example/goodreads/exceptions/NullPointerException.java
+++
@@ -1,9 +0,0 @@
-package com.example.goodreads.exceptions;
-
-public class NullPointerException extends RuntimeException {
-
-    public NullPointerException(String msg){
-        super(msg);
-    }
-
-}
