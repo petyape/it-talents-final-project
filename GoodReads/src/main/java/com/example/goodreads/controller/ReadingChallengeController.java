f6f24cbcd8a824e21e6a234241770642158dffcf
Sanya Sevdeva <s.g.sevdeva@gmail.com>
2022-02-24 12:47:16 +0200

    Created getParticipants functionality

--- GoodReads/src/main/java/com/example/goodreads/controller/ReadingChallengeController.java
+++ GoodReads/src/main/java/com/example/goodreads/controller/ReadingChallengeController.java
@@ -1,18 +1,22 @@
 package com.example.goodreads.controller;
 
+import com.example.goodreads.model.dto.quoteDTO.QuoteResponseDTO;
 import com.example.goodreads.model.dto.readingChallengeDTO.ChallengeResponseDTO;
 import com.example.goodreads.model.dto.readingChallengeDTO.EnterChallengeDTO;
+import com.example.goodreads.model.dto.readingChallengeDTO.ParticipantDTO;
 import com.example.goodreads.model.entities.ReadingChallenge;
 import com.example.goodreads.services.ReadingChallengeService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 public class ReadingChallengeController extends BaseController {
@@ -31,4 +35,17 @@ public class ReadingChallengeController extends BaseController {
         return ResponseEntity.ok(dto);
     }
 
+    @PutMapping("/challenge/quit")
+    public ResponseEntity<String> enterChallenge(HttpSession session, HttpServletRequest request) {
+        validateSession(session, request);
+        String msg = challengeService.quitChallenge((long) session.getAttribute(USER_ID));
+        return ResponseEntity.ok(msg);
+    }
+
+    @GetMapping("/challenge/participants")
+    public ResponseEntity<List<ParticipantDTO>> getParticipants(HttpSession session, HttpServletRequest request) {
+        validateSession(session, request);
+        List<ParticipantDTO> dtoList = challengeService.getParticipants();
+        return ResponseEntity.ok(dtoList);
+    }
 }
--- GoodReads/src/main/java/com/example/goodreads/services/ReadingChallengeService.java
+++ GoodReads/src/main/java/com/example/goodreads/services/ReadingChallengeService.java
@@ -3,13 +3,16 @@ package com.example.goodreads.services;
 import com.example.goodreads.exceptions.BadRequestException;
 import com.example.goodreads.exceptions.NotFoundException;
 import com.example.goodreads.model.dto.readingChallengeDTO.EnterChallengeDTO;
+import com.example.goodreads.model.dto.readingChallengeDTO.ParticipantDTO;
 import com.example.goodreads.model.entities.ReadingChallenge;
 import com.example.goodreads.model.entities.User;
 import com.example.goodreads.model.repository.ReadingChallengeRepository;
 import com.example.goodreads.model.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +21,8 @@ public class ReadingChallengeService {
     private ReadingChallengeRepository readingChallengeRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ModelMapper mapper;
 
     public ReadingChallenge enterChallenge(EnterChallengeDTO entryRequest, long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> (new NotFoundException("User not found!")));
@@ -36,4 +41,24 @@ public class ReadingChallengeService {
         return readingChallengeRepository.save(entry);
     }
 
+    public String quitChallenge(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> (new NotFoundException("User not found!")));
+        Optional<ReadingChallenge> opt = readingChallengeRepository.findReadingChallengeByUser(user);
+        if (opt.isEmpty()) {
+            throw new BadRequestException("User does not participate in challenge!");
+        }
+        readingChallengeRepository.delete(opt.get());
+        return "Successfully deleted challenge entry with id " + opt.get().getEntryId() + ".";
+    }
+
+    public List<ParticipantDTO> getParticipants() {
+        List<ReadingChallenge> entries = readingChallengeRepository.findAll();
+        List<ParticipantDTO> dtoList = new ArrayList<>();
+        for (ReadingChallenge entry : entries) {
+            ParticipantDTO dto = mapper.map(entry, ParticipantDTO.class);
+            dto.setFirstName(entry.getUser().getFirstName());
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
 }
